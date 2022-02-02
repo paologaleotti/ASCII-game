0e7b85eb54250ff6bdf087faa82a8a8502bb0aeb
paologaleotti <paologaleotti2002@gmail.com>
2022-02-02 14:05:37 +0100

    added comments in Graphics module

--- src/Graphics.hpp
+++ src/Graphics.hpp
@@ -3,14 +3,21 @@
 
 class MainWindow {
 private:
+	// caratteri per la stampa della matrice
 	char wallCh;
 	char playerCh;
 	char enemyCh;
-
+	
+	// traduce int nel giusto carattere
 	char matrix_translate(int toConvert);
 
 public:
+	// costruttore
 	MainWindow(char wallch, char playerch, char enemych);
+	
+	// stampa la matrice
     void print_room(Room *room, Player *player);
+	
+	// scambia la stanza con un'altra data come parametro
 	void swap_room(Room *room);
 };
